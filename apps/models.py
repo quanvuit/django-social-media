76738d14b495e67f9e219447b42515f242e23951
Quan Vu Van <quan.vuvan@glinteco.com>
2023-02-25 17:30:53 +0700

    Change models

--- apps/models.py
+++ apps/models.py
@@ -42,38 +42,3 @@ class Friendship(models.Model):
 
     def __str__(self):
         return self.user.username
-
-# class Profile(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     id_user = models.IntegerField()
-#     bio = models.TextField(blank=True)
-#     profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
-#     location = models.CharField(max_length=256, blank=True)
-
-#     def __str__(self):
-#         return self.user.username
-
-# class Post(models.Model):
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-#     user = models.CharField(max_length=256)
-#     image = models.ImageField(upload_to='post_images')
-#     caption = models.TextField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     no_of_likes = models.IntegerField(default=0)
-
-#     def __str__(self):
-#         return self.user
-
-# class LikePost(models.Model):
-#     post_id = models.CharField(max_length=256)
-#     username = models.CharField(max_length=256)
-
-#     def __str__(self):
-#         return self.username
-
-# class FollowersCount(models.Model):
-#     follower = models.CharField(max_length=256)
-#     user = models.CharField(max_length=256)
-
-#     def __str__(self):
-#         return self.user 
